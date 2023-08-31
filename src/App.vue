10e0eab18f6df2621c4a84386719e0b53fc228eb
Jefferson Coelho <bio.jeffcoelho@gmail.com>
2023-08-30 21:25:22 -0400

    config: ⚙️ Changing configuration files.

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,22 @@
-<script setup lang="ts">
-import RegisterActivityDialog from './components/RegisterActivityDialog.vue'
-</script>
+<script setup lang="ts"></script>
 
 <template>
 	<v-app>
-		<RegisterActivityDialog />
+		<v-main>
+			<router-view id="content" />
+		</v-main>
 	</v-app>
 </template>
+
+<style>
+#content {
+	position: absolute;
+	top: 50%;
+	left: 50%;
+	transform: translate(-50%, -50%);
+	border: 1px solid lightgray;
+	padding: 4rem 4rem;
+	border-radius: 5px;
+	background: #fefefe;
+}
+</style>
--- src/main.ts
+++ src/main.ts
@@ -1,25 +1,9 @@
 import { createApp } from 'vue'
-//import "./style.css";
 import App from './App.vue'
+import { registerPlugins } from './plugins/index'
 
-// Vuetify
-import 'vuetify/styles'
-import { createVuetify } from 'vuetify'
-import * as components from 'vuetify/components'
-import * as directives from 'vuetify/directives'
+const app = createApp(App)
 
-import { aliases, mdi } from 'vuetify/iconsets/mdi'
+registerPlugins(app)
 
-const vuetify = createVuetify({
-	components,
-	directives,
-	icons: {
-		defaultSet: 'mdi',
-		aliases,
-		sets: {
-			mdi
-		}
-	}
-})
-
-createApp(App).use(vuetify).mount('#app')
+app.mount('#app')
