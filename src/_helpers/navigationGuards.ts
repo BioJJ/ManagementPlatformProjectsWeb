1820e00a332c7a27abf179d219f56558b9b13817
Jefferson Coelho <bio.jeffcoelho@gmail.com>
2023-08-30 23:00:22 -0400

    feat: ✨ add _helpers

---
+++ src/_helpers/navigationGuards.ts
@@ -0,0 +1,48 @@
+import { RouteLocationNormalized } from 'vue-router'
+import { useAuthStore } from './../stores/auth.store'
+import { AuthService } from '../services/auth.service'
+import { decodeToken } from './utils'
+
+export function authGuard(
+	to: RouteLocationNormalized,
+	from: RouteLocationNormalized,
+	// eslint-disable-next-line @typescript-eslint/no-explicit-any
+	next: any
+) {
+	const service = new AuthService()
+	const store = useAuthStore()
+	if (store.user === null) {
+		if (localStorage.getItem('accessToken')) {
+			const token = JSON.stringify(localStorage.getItem('accessToken'))
+			if (decodeToken(token).exp < Math.round(new Date().getTime() / 1000)) {
+				if (to.path == '/login') return next()
+				return next('/login')
+			}
+			store.setToken(JSON.parse(token))
+
+			if (localStorage.getItem('userName')) {
+				store.userName = JSON.parse(localStorage.getItem('userName') as string)
+				return service
+					.checkToken(store.userName)
+					.then((payload) => {
+						caches.delete('check')
+						store.userChecked(payload.data)
+						if (to.path == '/login') return next('/home')
+						return next()
+					})
+					.catch(() => {
+						return next('/login')
+					})
+			}
+		} else {
+			// Não tem token
+			if (to.path == '/login') return next()
+			return next('/login')
+		}
+	} else {
+		if (to.path == '/login') {
+			return next(from.path)
+		}
+		return next()
+	}
+}
