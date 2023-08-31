9d38154a034b52560fd1addbf5770b4fa4ad7b07
Jefferson Coelho <bio.jeffcoelho@gmail.com>
2023-08-31 01:19:08 -0400

    feat: ✨ add init views

---
+++ src/views/activities/Activities.vue
@@ -0,0 +1,13 @@
+<template>
+	<h1>Clients</h1>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+export default defineComponent({
+	// eslint-disable-next-line vue/multi-word-component-names
+	name: 'Activities'
+})
+</script>
+
+<style></style>
